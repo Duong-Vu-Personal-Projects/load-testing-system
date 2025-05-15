ee8c0243a12fbeaaeebe0ae35f9ee5ddce99cf66
Duong Vu Cong Tuan <DuongVCT.B22KH024@stu.ptit.edu.vn>
2025-05-15 07:07:12 +0700

    Add backend setup folder

---
+++ backend/src/test/java/com/vn/ptit/duongvct/LoadTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vn.ptit.duongvct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
